5df38d156978d17329a265b54ad75ee34054db3e
andonius <34992832+alang321@users.noreply.github.com>
2020-06-09 11:30:06 +0200

    minor

--- main.py
+++ main.py
@@ -1,51 +1,11 @@
-import pygame
-from settings import gamesettings
-from scenes.mainmenuscene import mainmenuscene
-from background import background
+from placeholder import placeholder
 
-class main:
-    #path for the settings
-    settingspath = "./settings.txt"
+# Left right with a and d
+# switch item q and e
+# aim with mouse
+# shoot with space bar
+# escape to go back
 
-    def __init__(self):
-        self.settings = gamesettings(self.settingspath)
+# get a nuke by getting a kill (or switch to infinite in settings)
 
-        # initialize pygame
-        pygame.init()
-        pygame.font.init()
-        pygame.mixer.init()
-
-        # screen
-        if self.settings.gamevalues["Fullscreen"]:
-            self.screen = pygame.display.set_mode(self.settings.gamevalues["Resolution"], pygame.FULLSCREEN)
-        else:
-            self.screen = pygame.display.set_mode(self.settings.gamevalues["Resolution"])
-
-        self.background = background(self.settings.gamevalues["Resolution"], (19, 19, 39, 255))
-
-        self.defaultscene = mainmenuscene
-
-        self.currentscene = self.defaultscene
-
-
-
-        self.sceneswitcher()
-
-
-    def sceneswitcher(self):
-        running = True
-
-        #extra arguments that can be passed
-        args = []
-
-        while running:
-            scene = self.currentscene(self.screen, self.background, self.settings, *args)
-
-            #switch to next scene or quit if there is no next scene
-            if scene.nextscene is None:
-                self.currentscene = self.defaultscene
-                args = []
-            else:
-                self.currentscene = scene.nextscene
-                args = scene.arguments
-        pygame.quit()
+placeholder()
